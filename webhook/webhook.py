c5538174bb6538080d90030b1e45104ef7d5d17e
Pietu <pietu@smartly.io>
2018-08-07 09:17:58 -0700

    Add docker

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -1,6 +1,7 @@
 import logging
 import config
 from telegram.ext import Updater
+import telegram.error
 
 # Logging
 logging.basicConfig(
@@ -9,4 +10,21 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-updater = Updater(token=config.TELEGRAM_TOKEN)
+if (config.PRODUCTION):
+    logging.info("running in PRODUCTION")
+logging.info("running {}".format(config.TELEGRAM_TOKEN))
+
+try:
+    updater = Updater(token=config.TELEGRAM_TOKEN)
+    logging.info(
+        "Starting webhook on {}<token>".format(
+            config.TELEGRAM_DOMAIN))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=config.TELEGRAM_PORT,
+                          url_path=congig.TELEGRAM_TOKEN)
+    updater.bot.set_webhook(
+        config.TELEGRAM_DOMAIN +
+        congig.TELEGRAM_TOKEN)
+    updater.idle()
+except telegram.error.InvalidToken as err:
+    logging.error(err)
