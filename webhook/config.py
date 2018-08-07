e21a37db434733190ed73bdf3bc1a0e8c1280d24
Pietu <pietu@smartly.io>
2018-08-07 09:07:40 -0700

    Add config.py

--- webhook/config.py
+++ webhook/config.py
@@ -2,4 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+PRODUCTION = int(os.getenv("PRODUCTION"))
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_PORT = os.getenv("TELEGRAM_PORT")
+TELEGRAM_DOMAIN = os.getenv("TELEGRAM_DOMAIN")
