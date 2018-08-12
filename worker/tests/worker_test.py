52be1d4f70736147b6e64443feebcfaf2357b7f7
Pietu <pietu@smartly.io>
2018-08-12 15:48:19 -0700

    Basic unit test framework

---
+++ worker/tests/worker_test.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import unittest
+import testing.postgresql
+
+# Logging
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Postgresql class which shares the generated test database
+Postgresql = testing.postgresql.PostgresqlFactory(cache_initialized_db=True)
+
+# A hack to make imports work in the test target
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+
+# Test target
+import worker
+
+
+def tearDownModule(self):
+    # clear cached database at end of tests
+    Postgresql.clear_cache()
+
+
+class TestRegisterFlow(unittest.TestCase):
+    def setUp(self):
+        self.db = Postgresql()
+        self.redis = None
+        self.worker = worker
+        self.worker.bind(self.db, self.redis)
+
+    def tearDown(self):
+        self.postgresql.stop()
+
+    def test_dummy(self):
+        result = self.worker.handle_update(None, {"test": True})
+        self.assertEqual(result, 'FOO')
