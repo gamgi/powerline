419390580d54f187de63eba76d87112c109fb2bc
Pietu <pietu@smartly.io>
2018-08-16 22:27:43 -0700

    Make worker a class

--- worker/worker.py
+++ worker/worker.py
@@ -1,12 +1,12 @@
 # Telegram API
 import telegram.error
 # State machine
-import state
+from state import State
 # from telegram import Bot
 import config
 # Database
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, OperationalError
 import models
 # Other
 import enums
@@ -20,30 +20,69 @@ logger = logging.getLogger("worker.internal")
 logger.setLevel(logging.INFO)
 
 
-def command_start(user_id, update):
-    logging.debug("command_start ({})".format(user_id))
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=enums.MESSAGES['EN'].START_HELLO_MESSAGE)
-    # Save user to DB if new
-    session = Session()
-    user = session.query(models.User).get(user_id)
-    if (not user):
+class Worker:
+    def bind(self, bot, db_engine, redis):
+        self.bot = bot
+        self.db = db_engine
+        self.redis = redis
+        self.Session = sessionmaker(bind=self.db)
+        self.state = State()
+
+    def command_start(self, user_id, update):
+        logging.debug("command_start ({})".format(user_id))
+        self.bot.send_message(
+            chat_id=update.message.chat_id,
+            text=enums.MESSAGES['EN'].START_HELLO_MESSAGE.value)
+        # Save user to DB if new
         try:
-            user = models.User(
-                id=user_id,
-                username=update.message.chat.username,
-                first_name=update.message.chat.first_name)
-            session.add(user)
-            session.commit()
-        except SQLAlchemyError as e:
-            logging.error(e)
-    session.close()
+            session = self.Session()
+            user = session.query(models.User).get(user_id)
+            if (not user):
+                try:
+                    user = models.User(
+                        id=user_id,
+                        username=update.message.chat.username,
+                        first_name=update.message.chat.first_name)
+                    session.add(user)
+                    session.commit()
+                except SQLAlchemyError as e:
+                    logging.error(e)
+            session.close()
+        except OperationalError:
+            logging.error("Unable to connect to database")
+        # state.user.trigger("next")
+
+    def handle_command(self, user_id, update, command, args):
+        chat_id = update.message.chat_id
+        # Check is registered
+        # Check state nad set self.state.set_state('unregistered')
+        logging.info(
+            "command {} with args {} ({})".format(
+                command, args, user_id))
+        try:
+            self.state.trigger(command)
+            logging.info('new state is {}'.format(self.state.state))
+            # success, make menu of possible commands via
+            # m.get_triggers(self.state.state)
+        except Exception as err:
+            logging.error(err)
 
 
-def bind(new_bot, new_db_engine, new_redis):
-    global bot, db, redis, Session
-    bot = new_bot
-    db = new_db_engine
-    redis = new_redis
-    Session = sessionmaker()
-    Session.configure(bind=db)
+# Hacky-solution for production. Python-rq does not allow queueing instances, but
+# instantiating the worker in the module is allowed.
+# Having the worker as a class makes testing easier.
+
+
+worker = Worker()
+
+
+def command_start(*args):
+    worker.command_start(*args)
+
+
+def handle_command(*args):
+    worker.handle_command(*args)
+
+
+def bind(*args):
+    worker.bind(*args)
