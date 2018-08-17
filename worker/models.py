0178757fa5f43c0039f2146dd56633ed293caa79
Pietu <pietu@smartly.io>
2018-08-16 22:29:25 -0700

    Add user model

---
+++ worker/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, String, Integer, func, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    username = Column(String, nullable=False)
+    registered = Column(DateTime, default=func.now(), nullable=False)
+    #chat_id = Column(String, nullable=False)
+
+    def __repr__(self):
+        return 'user: {}'.format(self.id, self.username)
