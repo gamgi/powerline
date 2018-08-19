4e9e99cdea868ed9e5f3c10ad0f21fb31a7ea5be
Pietu <pietu@smartly.io>
2018-08-19 10:22:26 -0700

    Save user to db after_state_change

--- worker/worker.py
+++ worker/worker.py
@@ -20,12 +20,17 @@ logger = logging.getLogger("worker.internal")
 logger.setLevel(logging.INFO)
 
 
+class UserNotFoundError(LookupError):
+    pass
+
+
 class Worker:
     def bind(self, bot, Session, redis):
         self.bot = bot
         self.redis = redis
         self.Session = Session
         self.state = State(self.bot)
+        logging.info('Worker bound')
 
     def handle_command_start(self, user_id, update):
         logging.debug("command_start ({})".format(user_id))
@@ -50,40 +55,78 @@ class Worker:
             session.close()
         except OperationalError:
             logging.error("Unable to connect to database")
-        self.state.trigger("start", user_id=user_id, chat_id=chat_id)
+        success = self.state.trigger("start", user_id=user_id, chat_id=chat_id)
+        if success:
+            user.state = self.state.state
+            self.save_user(user)
+        logging.info('new state is {}'.format(self.state.state))
 
     def handle_command(self, user_id, update, command, args):
         chat_id = update.message.chat_id
-        logging.info('message from chat_id {}'.format(chat_id))
+        user = self.get_user(user_id)
         # Check is registered
         # Check state nad set self.state.set_state('unregistered')
         logging.info(
             "command {} with args {} ({})".format(
                 command, args, user_id))
         try:
-            self.state.trigger(command, user_id=user_id, chat_id=chat_id)
+            success = self.state.trigger(
+                command,
+                user_id=user_id,
+                chat_id=chat_id,
+                args=args)
             logging.info('new state is {}'.format(self.state.state))
+            if success:
+                user.state = self.state.state
+                self.save_user(user)
+                #self.save_state(user_id, self.state.state)
             # success, make menu of possible commands via
             # m.get_triggers(self.state.state)
-        except Exception as err:
+        except AttributeError as err:
             logging.error(err)
 
     def handle_message(self, user_id, update, message):
         chat_id = update.message.chat_id
-        # Check is registered
-        # Check state nad set self.state.set_state('unregistered')
+        #try:
+        user = self.get_user(user_id)
+        #except Exception as err:
+        #    logging.error(err)
+        #    return err
+            # TODO
+        self.state.set_state(user.state)
 
         logging.info(
-            "message {} ({})".format(
+            "message \"{}\" ({})".format(
                 message, user_id))
         try:
-            pass
-            # self.state.trigger(command)
-            #logging.info('new state is {}'.format(self.state.state))
-            # success, make menu of possible commands via
-            # m.get_triggers(self.state.state)
-        except Exception as err:
+            success = self.state.trigger("message", user_id=user_id, chat_id=chat_id, message=message)
+        except AttributeError as err:
             logging.error(err)
+            raise
+
+    def get_user(self, user_id):
+        try:
+            session = self.Session()
+            user = session.query(models.User).get(user_id)
+            if (not user):
+                raise UserNotFoundError
+            session.close()
+            return user
+        except OperationalError:
+            logging.error("Unable to connect to database")
+
+    def save_user(self, user):
+        try:
+            session = self.Session()
+            try:
+                session.add(user)
+                session.commit()
+            except SQLAlchemyError as e:
+                logging.error(e)
+            session.close()
+        except OperationalError:
+            logging.error("Unable to connect to database")
+
 
 # Hacky-solution for production. Python-rq does not allow queueing instances, but
 # instantiating the worker in the module is allowed.
