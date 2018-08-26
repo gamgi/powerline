bb1ce21530b5c3dafca32c4c7c49c607cdb1d422
Pietu <pietu@smartly.io>
2018-08-26 14:34:44 -0700

    clean up model

--- worker/models.py
+++ worker/models.py
@@ -10,24 +10,12 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=False)
     first_name = Column(String)
     username = Column(String, nullable=False)
-    #registered = Column(DateTime, default=func.now(), nullable=False)
     state = Column(String, default='unregistered', nullable=False)
+    tos_accepted = Column(String)
+
     title = Column(String)
     age = Column(String)
     subscription = Column(String, default='normal')
-    #chat_id = Column(String, nullable=False)
-
-    """
-    @validates('id')
-    def validate_id(self, key, id):
-        assert id != None
-        return id
-
-    @validates('state')
-    def validate_id(self, key, state):
-        assert state != None
-        return state
-    """
 
     def __repr__(self):
         return 'user: {}'.format(id)
